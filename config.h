ee511e47f69222979c48141385234a9181f44c8c
The Lambster <97347052+TheLambster@users.noreply.github.com>
2024-11-18 16:23:17 -0600

    Update config.h

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#000000";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I" };
 
 static const Rule rules[] = {
 	/* xprop(1):
